04776d051b7340a47655e66fd95f4d1c350cebf7
likunyan <lky216@gmail.com>
2020-02-25 10:41:21 +0800

    Update Demo.js

--- src/containers/Demo.js
+++ src/containers/Demo.js
@@ -5,11 +5,11 @@ const Demo = () => {
   return (
     <>
       <ul>
-        <li><a href="/demos/threejs/3d" target="_blank">Emoji 3D(Three.js)</a></li>
-        <li><a href="/demos/threejs/love" target="_blank">喜欢的（Three.js）</a></li>
-        <li><a href="/demos/threejs/threejs_load_images" target="_blank">Three.js 加载图片</a></li>
+        <li><a href="/demos/threejs/3d.html" target="_blank">Emoji 3D(Three.js)</a></li>
+        <li><a href="/demos/threejs/love.html" target="_blank">喜欢的（Three.js）</a></li>
+        <li><a href="/demos/threejs/threejs_load_images.html" target="_blank">Three.js 加载图片</a></li>
         <li><a href="/demos/mv.html" target="_blank">MV 测试</a></li>
-        <li><a href="/demos/react-test" target="_blank">React 单文件测试</a></li>
+        <li><a href="/demos/react-test.html" target="_blank">React 单文件测试</a></li>
         {/*<li><a href="/demos/tailwind" target="_blank">Tailwind CSS 学习</a></li>*/}
         <li><Link to="/demos/clock">Time</Link></li>
         <li><Link to="/demos/calculator">🌡摄氏度、华氏度转换（React 官方 Example）</Link></li>
