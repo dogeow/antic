e05b8b5e02e393083003f0165acacb070b26800a
likunyan <lky216@gmail.com>
2020-03-04 00:04:36 +0800

    字体测试

---
+++ src/demo/Font.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+
+
+const Font = () => (
+  <div>
+    <Typography variant="h1" component="h2">
+      我的 English 书本，购入于 2020-02-02
+    </Typography>
+    <Typography variant="h2" component="h2">
+      我的 English 书本，购入于 2020-02-02
+    </Typography>
+    <Typography variant="h3" component="h2">
+      我的 English 书本，购入于 2020-02-02
+    </Typography>
+    <Typography variant="h4" component="h2">
+      我的 English 书本，购入于 2020-02-02
+    </Typography>
+    <Typography variant="h5" component="h2">
+      我的 English 书本，购入于 2020-02-02
+    </Typography>
+    <Typography variant="h6" component="h2">
+      我的 English 书本，购入于 2020-02-02
+    </Typography>
+    <Typography variant="subtitle1" component="h2">
+      我的 English 书本，购入于 2020-02-02
+    </Typography>
+    <Typography variant="subtitle2" component="h2">
+      我的 English 书本，购入于 2020-02-02
+    </Typography>
+    <Typography variant="body1" component="h2">
+      我的 English 书本，购入于 2020-02-02
+    </Typography>
+    <Typography variant="body2" component="h2">
+      我的 English 书本，购入于 2020-02-02
+    </Typography>
+    <Typography variant="caption" component="h2">
+      我的 English 书本，购入于 2020-02-02
+    </Typography>
+    <Typography variant="overline" component="h2">
+      我的 English 书本，购入于 2020-02-02
+    </Typography>
+  </div>
+);
+
+export default Font;
