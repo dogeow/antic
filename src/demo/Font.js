4a564da2f0bcc4500e6219de2182e3397c3f6fb8
likunyan <lky216@gmail.com>
2020-03-04 17:40:33 +0800

    Update Font.js

--- src/demo/Font.js
+++ src/demo/Font.js
@@ -4,40 +4,40 @@ import Typography from '@material-ui/core/Typography'
 const Font = () => (
   <div>
     <Typography variant="h1" component="h2">
-      我的 English 书本，购入于 2020-02-02
+      世界、🌍，By 2020
     </Typography>
     <Typography variant="h2" component="h2">
-      我的 English 书本，购入于 2020-02-02
+      世界、🌍，By 2020
     </Typography>
     <Typography variant="h3" component="h2">
-      我的 English 书本，购入于 2020-02-02
+      世界、🌍，By 2020
     </Typography>
     <Typography variant="h4" component="h2">
-      我的 English 书本，购入于 2020-02-02
+      世界、🌍，By 2020
     </Typography>
     <Typography variant="h5" component="h2">
-      我的 English 书本，购入于 2020-02-02
+      世界、🌍，By 2020
     </Typography>
     <Typography variant="h6" component="h2">
-      我的 English 书本，购入于 2020-02-02
+      世界、🌍，By 2020
     </Typography>
     <Typography variant="subtitle1" component="h2">
-      我的 English 书本，购入于 2020-02-02
+      世界、🌍，By 2020
     </Typography>
     <Typography variant="subtitle2" component="h2">
-      我的 English 书本，购入于 2020-02-02
+      世界、🌍，By 2020
     </Typography>
     <Typography variant="body1" component="h2">
-      我的 English 书本，购入于 2020-02-02
+      世界、🌍，By 2020
     </Typography>
     <Typography variant="body2" component="h2">
-      我的 English 书本，购入于 2020-02-02
+      世界、🌍，By 2020
     </Typography>
     <Typography variant="caption" component="h2">
-      我的 English 书本，购入于 2020-02-02
+      世界、🌍，By 2020
     </Typography>
     <Typography variant="overline" component="h2">
-      我的 English 书本，购入于 2020-02-02
+      世界、🌍，By 2020
     </Typography>
   </div>
 );
