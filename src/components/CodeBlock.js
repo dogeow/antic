56e96b5a92907a4413abc75a2de8fdcccc58a9cf
likunyan <lky216@gmail.com>
2020-03-05 20:53:01 +0800

    Update CodeBlock.js

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -5,7 +5,7 @@ import { coy } from "react-syntax-highlighter/dist/esm/styles/prism";
 // 设置高亮的语言
 import { jsx, javascript } from "react-syntax-highlighter/dist/esm/languages/prism";
 
-const CodeBlock = ({language = null, value}) => {
+const CodeBlock = ({language = 'php', value}) => {
   useEffect(() => {
     // 注册要高亮的语法，
     // 注意：如果不设置打包后供第三方使用是不起作用的
