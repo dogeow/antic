9f570e637094f9c2e963603a8386b5aef59f22d1
likunyan <lky216@gmail.com>
2020-03-05 15:11:42 +0800

    233

--- src/containers/Demo.js
+++ src/containers/Demo.js
@@ -8,6 +8,7 @@ const Demo = () => (
         <li><a href="/demos/threejs/love.html" target="_blank">喜欢的（Three.js）</a></li>
         <li><a href="/demos/threejs/threejs_load_images.html" target="_blank">Three.js 加载图片</a></li>
         <li><a href="/demos/mv.html" target="_blank">MV 测试</a></li>
+        <li><a href="/demos/jqueryselectors" target="_blank">jQuery 选择器在线测试</a></li>
         <li><a href="/demos/react-test.html" target="_blank">React 单文件测试</a></li>
         <li><a href="/demos/tailwind" target="_blank">Tailwind CSS 学习</a></li>
         <li><Link to="/demos/clock">Time</Link></li>
