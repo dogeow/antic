6b2df448003be41cb465bc5463639f3a8ed3d482
likunyan <lky216@gmail.com>
2020-03-05 22:23:47 +0800

    233

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import store from "./store";
-import './index.css'
 import App from './containers/App';
 import Wrap from './middleware/lifecycle'
 import * as serviceWorker from './serviceWorker';
