4d374455c624c92e59524943bba67e320e747acc
likunyan <lky216@gmail.com>
2020-03-05 21:34:46 +0800

    Update CodeBlock.js

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -1,9 +1,6 @@
 import React from "react";
 import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/prism-light";
-
-// 设置高亮样式
 import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
-// 设置高亮的语言
 import { jsx, javascript, php, bash, css, json, nginx } from "react-syntax-highlighter/dist/esm/languages/prism";
 
 SyntaxHighlighter.registerLanguage("jsx", jsx);
