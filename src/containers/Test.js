5c9c10183150d7b4f6d4fcdd13b15a96ead29ef5
likunyan <lky216@gmail.com>
2020-03-15 15:31:54 +0800

    Update Test.js

--- src/containers/Test.js
+++ src/containers/Test.js
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios'
 
-const Test = () => (<></>);
+const Test = () => {
+  useEffect(() => {
+    axios.post('http://api.heclouds.com/devices/23232/lbs/latestLocation', {
+      headers: {
+        'User-Agent': '2333',
+        'api-key': '2w4*****y5Y=',
+      }
+    })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  return (<>233</>);
+};
 
 export default Test
