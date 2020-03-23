c11f3ba2ba5b391f5062426048d90444e3122eaf
likunyan <lky216@gmail.com>
2020-03-23 22:47:19 +0800

    Update Spa.js

--- src/routes/Spa.js
+++ src/routes/Spa.js
@@ -6,30 +6,9 @@ import Container from '@material-ui/core/Container';
 import LocalAirportIcon from '@material-ui/icons/LocalAirport';
 
 // Pages
-import Register from '../containers/Register';
 import Index from '../containers/Index';
-import About from '../containers/About';
-import Api from '../containers/Api';
-import Todo from '../components/Todo/ProjectsList';
-import CreateTodoProject from '../components/Todo/NewProject';
-import TodoProject from '../components/Todo/SingleProject';
-import WeiboAbout from '../containers/WeiboAbout';
-import SelfTalk from '../containers/SelfTalk';
-import EmojiPost from '../components/Emoji/EmojiPost';
-import User from '../containers/User';
-import Footer from '../containers/Footer';
-
-// Layouts
 import Header from '../components/Header';
-import NoMatch from '../containers/NoMatch';
-
-// Demos
-import Clock from '../demo/Clock';
-import Calculator from '../demo/Calculator/';
-import Chess from '../containers/Chess';
-import Font from '../demo/Font';
-
-import Test from '../containers/Test';
+import Footer from '../containers/Footer';
 
 const useStyles = makeStyles(theme => ({
   main: {
@@ -53,46 +32,66 @@ const Spa = ({match}) => {
                  component={loadable(() => import('../containers/Game'))}/>
           <Route path="/algolia"
                  component={loadable(() => import('../containers/Algolia'))}/>
-          <Route exact path="/post" component={loadable(() => import('../containers/Post'))}/>
-          <Route path="/post/create" component={loadable(() => import('../components/PostCreate'))}/>
-          <Route path="/post/:id/edit" component={loadable(() => import('../components/PostCreate'))}/>
-          <Route path="/post/:id" component={loadable(() => import('../containers/PostSingle'))}/>
-          <Route path="/register" component={Register}/>
-          <Route path="/about" component={About}/>
-          <Route exact path="/api" component={Api}/>
+          <Route exact path="/post"
+                 component={loadable(() => import('../containers/Post'))}/>
+          <Route path="/post/create" component={loadable(
+            () => import('../components/PostCreate'))}/>
+          <Route path="/post/:id/edit" component={loadable(
+            () => import('../components/PostCreate'))}/>
+          <Route path="/post/:id" component={loadable(
+            () => import('../containers/PostSingle'))}/>
+          <Route path="/register"
+                 component={loadable(() => import('../containers/Register'))}/>
+          <Route exact path="/api"
+                 component={loadable(() => import('../containers/Api'))}/>
           <Route path="/sqg"
                  component={loadable(() => import('../components/Sqg'))}/>
-          <Route path="/about" component={About}/>
+          <Route path="/about"
+                 component={loadable(() => import('../containers/about'))}/>
           <Route exact path="/weibo"
                  component={loadable(() => import('../containers/Weibo'))}/>
-          <Route path="/weibo/about" component={WeiboAbout}/>
+          <Route path="/weibo/about" component={loadable(
+            () => import('../containers/WeiboAbout'))}/>
           <Route exact path="/emoji"
                  component={loadable(() => import('../components/Emoji'))}/>
-          <Route path="/emoji/post" component={EmojiPost}/>
-          <Route exact path="/todo" component={Todo}/>
-          <Route path="/todo/create" component={CreateTodoProject}/>
-          <Route path="/todo/:id" component={TodoProject}/>
-          <Route path="/self-talk" component={SelfTalk}/>
-          <Route path="/hall_of_fame" component={loadable(
-            () => import('../containers/HallOfFame'))}/>
-          <Route path="/nav" component={loadable(() => import('../components/Nav'))}/>
+          <Route path="/emoji/post" component={loadable(
+            () => import('../components/Emoji/EmojiPost'))}/>
+          <Route exact path="/todo" component={loadable(
+            () => import('../components/Todo/ProjectsList'))}/>
+          <Route path="/todo/create" component={loadable(
+            () => import('../components/Todo/NewProject'))}/>
+          <Route path="/todo/:id" component={loadable(
+            () => import('../components/Todo/SingleProject'))}/>
+          <Route path="/self-talk"
+                 component={loadable(() => import('../containers/SelfTalk'))}/>
+          <Route path="/hall_of_fame"
+                 component={loadable(
+                   () => import('../containers/HallOfFame'))}/>
+          <Route path="/nav"
+                 component={loadable(() => import('../components/Nav'))}/>
           <Route path="/piano"
                  component={loadable(() => import('../components/Piano'))}/>
           <Route path="/bookmarks"
                  component={loadable(() => import('../containers/Bookmarks'))}/>
           <Route path="/search"
                  component={loadable(() => import('../components/Search'))}/>
-          <Route path="/user/:id" component={User}/>
+          <Route path="/user/:id"
+                 component={loadable(() => import('../containers/User'))}/>
           <Route exact path="/demo"
                  component={loadable(() => import('../containers/Demo'))}/>
-          <Route path="/demos/chess" component={Chess}/>
-          <Route path="/demos/clock" component={Clock}/>
-          <Route path="/demos/calculator" component={Calculator}/>
+          <Route path="/demos/chess"
+                 component={loadable(() => import('../containers/Chess'))}/>
+          <Route path="/demos/clock"
+                 component={loadable(() => import('../demo/Clock'))}/>
+          <Route path="/demos/calculator"
+                 component={loadable(() => import('../demo/Calculator'))}/>
           <Route path="/demos/parking"
                  component={loadable(() => import('../containers/Parking'))}/>
-          <Route path="/demos/font" component={Font}/>
-          <Route path="/test" component={Test}/>
-          <Route component={NoMatch}/>
+          <Route path="/demos/font"
+                 component={loadable(() => import('../demo/Font'))}/>
+          <Route path="/test"
+                 component={loadable(() => import('../containers/Test'))}/>
+          <Route component={loadable(() => import('../containers/NoMatch'))}/>
         </Switch>
       </Container>
       {['/'].includes(match.url) && <Footer/>}
