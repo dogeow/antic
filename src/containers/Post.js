ce0e8c67ce4be1c02dab4c48ce592cc621d6fd39
likunyan <lky216@gmail.com>
2020-03-25 00:58:04 +0800

    分类和标签

--- src/containers/Post.js
+++ src/containers/Post.js
@@ -1,22 +1,21 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import Grid from '@material-ui/core/Grid'
-import {Link} from 'react-router-dom'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+import {Link} from 'react-router-dom';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
-import ImageIcon from '@material-ui/icons/Image';
+import Chip from '@material-ui/core/Chip';
 
 const Post = () => {
   const [post, setPost] = useState([]);
 
   useEffect(() => {
-    axios.get('post')
-      .then(response => {
-        setPost(response.data);
-      })
+    axios.get('post').then(response => {
+      setPost(response.data);
+    });
   }, []);
 
   return (
@@ -27,18 +26,27 @@ const Post = () => {
             <ListItem key={index}>
               <ListItemAvatar>
                 <Avatar>
-                  <ImageIcon />
+                  {item.categories[0].name}
                 </Avatar>
               </ListItemAvatar>
               <Link to={`/post/${item.id}`}>
-              <ListItemText primary={item.title} secondary={item.created_at} />
+                <ListItemText primary={item.title} secondary={
+                  item.tags.map((tag, index) => (
+                    <Chip
+                      variant="outlined"
+                      size="small"
+                      label={tag.name}
+                      key={index}/>
+                  ))
+                }
+                />
               </Link>
             </ListItem>
           ))
         }
       </List>
     </Grid>
-  )
+  );
 };
 
 export default Post;
