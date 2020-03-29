492c47ae6fd39d527a031a1aec397d5c69cf5300
likunyan <lky216@gmail.com>
2020-03-30 00:02:06 +0800

    Update Bookmarks.js

--- src/containers/Bookmarks.js
+++ src/containers/Bookmarks.js
@@ -1,53 +1,51 @@
-import React from 'react'
-import makeStyles from "@material-ui/core/styles/makeStyles";
-import TreeView from '@material-ui/lab/TreeView'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-import ChevronRightIcon from '@material-ui/icons/ChevronRight'
-import TreeItem from '@material-ui/lab/TreeItem'
-import chromeBookmarks from '../resources/Bookmarks'
+import React from 'react';
+import TreeView from '@material-ui/lab/TreeView';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import TreeItem from '@material-ui/lab/TreeItem';
+import chromeBookmarks from '../resources/Bookmarks';
 
 const subFolder = (project) => {
-  if ("folder" === project['type']) { // 文件夹
+  if ('folder' === project['type']) { // 文件夹
     return (
-      <TreeItem key={project['id']} nodeId={project['id']} label={project['name']}>
+      <TreeItem
+        key={project['id']}
+        nodeId={project['id']}
+        label={project['name']}
+      >
         {subFolder(project['children'])}
       </TreeItem>
     );
-  } else if ("url" === project['type']) { // 单个链接
+  } else if ('url' === project['type']) { // 单个链接
     return (
-      <a key={project['id']} href={project.url} target="_blank" rel="noopener noreferrer">
+      <a
+        key={project['id']}
+        href={project.url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <TreeItem nodeId={project['id']} label={project['name']}/>
       </a>
     );
   } else { // 子目录下的所有物件
     let array = [];
     project.map(children => array.push(subFolder(children)));
+
     return array;
   }
 };
 
-const useStyles = makeStyles({
-  root: {
-    height: 216,
-    flexGrow: 1,
-    maxWidth: 400,
-  },
-});
-
 const bookmarks = chromeBookmarks['roots']['bookmark_bar']['children'];
 
 const Bookmarks = () => {
-  const classes = useStyles();
-
   return (
     <TreeView
-      className={classes.root}
       defaultCollapseIcon={<ExpandMoreIcon/>}
       defaultExpandIcon={<ChevronRightIcon/>}
     >
       {bookmarks.map(children => subFolder(children))}
     </TreeView>
-  )
+  );
 };
 
 export default Bookmarks;
