a77d96a104d7f4633ecc82c261b0485b244d24d0
likunyan <lky216@gmail.com>
2020-03-29 23:14:49 +0800

    Update lab.js

--- src/reducers/lab.js
+++ src/reducers/lab.js
@@ -1,14 +1,13 @@
 import {isExpired} from '../helpers';
 
-let today = new Date();
-let now = today.getHours();
-let themePaletteType = ((now >= 18 && now <= 24) || (now >= 0 && now <= 6))
+let hour = new Date().getHours;
+let themePaletteType = (hour >= 18 && hour <= 24) || (hour >= 0 && hour <= 6)
   ? 'dark'
   : 'light';
 
 const defaultState = {
   toggle_drawer: false,
-  themePaletteType: themePaletteType,
+  themePaletteType,
   is_expired: isExpired(),
   access_token: localStorage.getItem('access_token') || null,
   user_id: localStorage.getItem('user_id') || null,
