f6c0026ebe4b1697de704c2212b891d257d66aac
likunyan <lky216@gmail.com>
2020-03-30 15:46:10 +0800

    交互

--- src/containers/PostHeader.js
+++ src/containers/PostHeader.js
@@ -1,26 +1,44 @@
-import Grid from "@material-ui/core/Grid";
-import Tooltip from "@material-ui/core/Tooltip";
-import moment from "moment";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
-import Button from "@material-ui/core/Button";
-import Skeleton from "@material-ui/lab/Skeleton";
-import React from "react";
+import Grid from '@material-ui/core/Grid';
+import Tooltip from '@material-ui/core/Tooltip';
+import moment from 'moment';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+import Skeleton from '@material-ui/lab/Skeleton';
+import React from 'react';
+import Zoom from '@material-ui/core/Zoom';
 
 const PostEdit = ({post, handleEdit}) => {
   return post ?
     <>
       <Grid item>
-        <Tooltip title={post.created_at} placement="top">
+        <Tooltip
+          title={post.created_at}
+          placement="top"
+          enterDelay={200}
+          TransitionComponent={Zoom}
+          interactive
+        >
           <div>创建于 <time>{moment(post.created_at).fromNow()}</time></div>
         </Tooltip>
       </Grid>
       <Grid item>
-        <Tooltip title={post.updated_at} placement="top">
+        <Tooltip
+          title={post.updated_at}
+          placement="top"
+          enterDelay={200}
+          TransitionComponent={Zoom}
+          interactive
+        >
           <div>更新于 <time>{moment(post.updated_at).fromNow()}</time></div>
         </Tooltip>
       </Grid>
       <Grid item>
-        <ButtonGroup size="small" variant="text" color="primary" aria-label="text primary button group">
+        <ButtonGroup
+          size="small"
+          variant="text"
+          color="primary"
+          aria-label="text primary button group"
+        >
           <Button onClick={handleEdit}>编辑</Button>
           <Button>删除</Button>
         </ButtonGroup>
@@ -31,7 +49,7 @@ const PostEdit = ({post, handleEdit}) => {
       <Grid item xs={12}>
         <Skeleton variant="rect" height={20}/>
       </Grid>
-    </>
+    </>;
 };
 
 export default PostEdit;
