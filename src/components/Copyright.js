447213137a2b9d23f8c353460abde025a98d6b5c
likunyan <lky216@gmail.com>
2020-04-01 01:02:27 +0800

    formate

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -1,10 +1,14 @@
-import { Link } from 'react-router-dom'
-import React from 'react'
-import Typography from '@material-ui/core/Typography'
+import {Link} from 'react-router-dom';
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
 const Copyright = () => (
   <Typography variant="body2" color="textSecondary" align="center">
-    {'Copyright © '}<Link to="/">滑稽实验室</Link>{' '}{new Date().getFullYear()}{'.'}
+    {'Copyright © '}
+    <Link
+      to="/">滑稽实验室
+    </Link>
+    {' '}{new Date().getFullYear()}{'.'}
   </Typography>
 );
 
