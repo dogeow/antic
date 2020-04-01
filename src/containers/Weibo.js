ae288af3938c2ad970fc2ddf941e6c87b3383cef
likunyan <lky216@gmail.com>
2020-04-01 19:05:28 +0800

    Update Weibo.js

--- src/containers/Weibo.js
+++ src/containers/Weibo.js
@@ -37,7 +37,7 @@ const Weibo = () => {
     axios.get('weibo', {
       date: moment(selectedDate).format('Y-MM-DD'),
     }).then(({data}) => {
-      setData(data);
+      setData(data.data);
     });
   }, [selectedDate]);
 
