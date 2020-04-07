cd48f07901893569c0a8c3f223ff4230242c1d77
likunyan <lky216@gmail.com>
2020-04-08 00:18:06 +0800

    去掉生命周期

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import store from './store';
 import App from './containers/App';
-import Wrap from './middleware/lifecycle';
 import * as serviceWorker from './serviceWorker';
-
-const WrapApp = Wrap(App); // 高阶组件。功能：显示生命周期
 
 ReactDOM.render(
   <Provider store={store}>
-    <WrapApp/>
+    <App/>
   </Provider>,
   document.getElementById('root'),
 );
