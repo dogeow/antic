d7dec4685c5e8ce532d6a149747480ecbbfb87e0
likunyan <lky216@gmail.com>
2020-04-08 00:09:01 +0800

    层次

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './store';
 import App from './containers/App';
 import Wrap from './middleware/lifecycle';
 import * as serviceWorker from './serviceWorker';
-import {Provider} from 'react-redux';
-import store from './store';
 
-const WrapApp = Wrap(App);
+const WrapApp = Wrap(App); // 高阶组件。功能：显示生命周期
+
 ReactDOM.render(
   <Provider store={store}>
     <WrapApp/>
