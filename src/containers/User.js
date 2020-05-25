c343cf5773eb410c3093a7cf6a666bd1fe1c6c91
likunyan <lky216@gmail.com>
2020-05-25 15:19:55 +0800

    Update User.js

--- src/containers/User.js
+++ src/containers/User.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useStore } from "react-redux";
 import md5 from "md5";
 import Grid from "@material-ui/core/Grid";
+import moment from 'moment';
 
 const User = () => {
   const store = useStore();
@@ -26,6 +27,12 @@ const User = () => {
       </Grid>
       <Grid item xs={12}>
         修改密码
+      </Grid>
+      <Grid item xs={12}>
+        <ul>
+          <li>User-Agent：{navigator.userAgent}</li>
+          <li>Token 到期时间：{moment.unix(localStorage.access_token_expired_at).format('YYYY-MM-DD HH:mm:ss')}</li>
+        </ul>
       </Grid>
     </Grid>
   );
