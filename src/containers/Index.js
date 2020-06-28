dbf25dcb47a988e3ab71cab8e5db7ce5aa001e71
likunyan <lky216@gmail.com>
2020-06-28 16:40:18 +0800

    Update Index.js

--- src/containers/Index.js
+++ src/containers/Index.js
@@ -1,15 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Grid from "@material-ui/core/Grid";
-import axios from "axios";
 import TodoList from "../components/TodoList";
 import PostList from "../components/PostList";
 
 const Index = () => {
-  useEffect(() => {
-    axios.post("http://127.0.0.1:8000/callback", {
-      test: "233",
-    });
-  });
   return (
     <Grid container spacing={2}>
       <Grid item xs={12} md>
