d5b624877ed65efd143487b33c74b5cc5983a99b
likunyan <lky216@gmail.com>
2020-08-04 11:22:58 +0800

    Update Settings.jsx

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -4,17 +4,9 @@ import Dialog from "@material-ui/core/Dialog";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import FormControl from "@material-ui/core/FormControl";
 import Switch from "@material-ui/core/Switch";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles(() => ({
-  root: {
-    padding: 10,
-  },
-}));
+import DialogContent from "@material-ui/core/DialogContent";
 
 const Settings = (props) => {
-  const classes = useStyles();
-
   function handleClose() {
     props.onClose();
   }
@@ -24,24 +16,23 @@ const Settings = (props) => {
       onClose={handleClose}
       aria-labelledby="simple-dialog"
       open={props.open}
-      classes={{
-        root: classes.root,
-      }}
     >
       <DialogTitle id="simple-dialog-title">设置</DialogTitle>
-      <FormControl>
-        <FormControlLabel
-          control={
-            <Switch
-              checked={props.themePaletteType === "dark"}
-              onClick={props.onThemeClick}
-              value="themePaletteType"
-              inputProps={{ "aria-label": "夜晚模式" }}
-            />
-          }
-          label="夜晚模式"
-        />
-      </FormControl>
+      <DialogContent>
+        <FormControl>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={props.themePaletteType === "dark"}
+                onClick={props.onThemeClick}
+                value="themePaletteType"
+                inputProps={{ "aria-label": "夜晚模式" }}
+              />
+            }
+            label="夜晚模式"
+          />
+        </FormControl>
+      </DialogContent>
     </Dialog>
   );
 };
