1a004e4a26b62f13e689bad920b39b4eb9277c33
likunyan <lky216@gmail.com>
2020-08-21 09:46:33 +0800

    谷歌翻译

--- src/containers/Index.jsx
+++ src/containers/Index.jsx
@@ -5,16 +5,19 @@ import PostList from "../components/PostList";
 
 const Index = () => {
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} md>
-        <h2>待办事项</h2>
-        <TodoList />
+    <>
+      <div id="google_translate_element" />
+      <Grid container spacing={2}>
+        <Grid item xs={12} md>
+          <h2>待办事项</h2>
+          <TodoList />
+        </Grid>
+        <Grid item xs={12} md>
+          <h2>文章</h2>
+          <PostList />
+        </Grid>
       </Grid>
-      <Grid item xs={12} md>
-        <h2>文章</h2>
-        <PostList />
-      </Grid>
-    </Grid>
+    </>
   );
 };
 
