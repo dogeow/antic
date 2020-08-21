43e5388e59a4c4a9e30d2cf0a670d18168a8acad
likunyan <lky216@gmail.com>
2020-08-21 14:45:21 +0800

    233

--- src/containers/Index.jsx
+++ src/containers/Index.jsx
@@ -2,19 +2,11 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import TodoList from "../components/TodoList";
 import PostList from "../components/PostList";
-import loadGoogleTranslate from "../components/loadGoogleTranslate";
 
 const Index = () => {
-  const [googleTranslate, setGoogleTranslate] = React.useState(false);
-  React.useEffect(() => {
-    loadGoogleTranslate(() => {
-      setGoogleTranslate(true);
-    });
-  });
-
   return (
     <>
-      {googleTranslate ? <div id="google_translate_element" /> : null}
+      <div id="google_translate_element" />
       <Grid container spacing={2}>
         <Grid item xs={12} md>
           <h2>待办事项</h2>
