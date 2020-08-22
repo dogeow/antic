a0cf54f8860a83bbb61bceb5eb2d21fee86e405d
likunyan <lky216@gmail.com>
2020-08-22 15:53:30 +0800

    首页不再显示文章

--- src/containers/Index.jsx
+++ src/containers/Index.jsx
@@ -1,21 +1,13 @@
-import Grid from "@material-ui/core/Grid";
 import React from "react";
 
-import PostList from "../components/PostList";
 import TodoList from "../components/TodoList";
 
 const Index = () => {
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} md>
-        <h2>待办事项</h2>
-        <TodoList />
-      </Grid>
-      <Grid item xs={12} md>
-        <h2>文章</h2>
-        <PostList />
-      </Grid>
-    </Grid>
+    <>
+      <h2>待办事项</h2>
+      <TodoList />
+    </>
   );
 };
 
