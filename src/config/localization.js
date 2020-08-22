019ae42102aefc19647f99a658b23c2d18e5e647
likunyan <lky216@gmail.com>
2020-08-22 19:26:16 +0800

    config

---
+++ src/config/localization.js
@@ -0,0 +1,38 @@
+export default {
+  pagination: {
+    labelRowsSelect: "行",
+    labelDisplayedRows: "{from}-{to} of {count}",
+    firstTooltip: "第一页",
+    firstAriaLabel: "第一页",
+    nextAriaLabel: "下一页",
+    nextTooltip: "下一页",
+    previousTooltip: "上一页",
+    previousAriaLabel: "上一页",
+    lastTooltip: "最后一页",
+    lastAriaLabel: "最后一页",
+  },
+  toolbar: {
+    nRowsSelected: "选择了 {0} 行",
+    searchTooltip: "搜索",
+    searchPlaceholder: "搜索",
+    exportTitle: "导出",
+    exportAriaLabel: "导出",
+  },
+  header: {
+    actions: "操作",
+  },
+  body: {
+    editTooltip: "编辑",
+    deleteTooltip: "删除",
+    addTooltip: "添加",
+    emptyDataSourceMessage: "无记录可显示",
+    editRow: {
+      saveTooltip: "保存",
+      cancelTooltip: "取消",
+      deleteText: "您确定要删除此行吗？",
+    },
+    filterRow: {
+      filterTooltip: "Filter",
+    },
+  },
+};
--- src/containers/PoweredBy.jsx
+++ src/containers/PoweredBy.jsx
@@ -1,6 +1,8 @@
 import MaterialTable from "material-table";
 import React, { useState } from "react";
 
+import localization from "../config/localization";
+
 export default function Table() {
   const [project, setProject] = useState([]);
 
@@ -101,44 +103,7 @@ export default function Table() {
               }, 1000);
             }),
         }}
-        localization={{
-          pagination: {
-            labelRowsSelect: "行",
-            labelDisplayedRows: "{from}-{to} of {count}",
-            firstTooltip: "第一页",
-            firstAriaLabel: "第一页",
-            nextAriaLabel: "下一页",
-            nextTooltip: "下一页",
-            previousTooltip: "上一页",
-            previousAriaLabel: "上一页",
-            lastTooltip: "最后一页",
-            lastAriaLabel: "最后一页",
-          },
-          toolbar: {
-            nRowsSelected: "选择了 {0} 行",
-            searchTooltip: "搜索",
-            searchPlaceholder: "搜索",
-            exportTitle: "导出",
-            exportAriaLabel: "导出",
-          },
-          header: {
-            actions: "操作",
-          },
-          body: {
-            editTooltip: "编辑",
-            deleteTooltip: "删除",
-            addTooltip: "添加",
-            emptyDataSourceMessage: "无记录可显示",
-            editRow: {
-              saveTooltip: "保存",
-              cancelTooltip: "取消",
-              deleteText: "您确定要删除此行吗？",
-            },
-            filterRow: {
-              filterTooltip: "Filter",
-            },
-          },
-        }}
+        localization={localization}
       />
       <div>
         <p>
