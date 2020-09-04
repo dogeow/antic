bca32f9caed4aab0e7753aac2583488f80c000b1
likunyan <lky216@gmail.com>
2020-09-04 23:55:22 +0800

    233

--- src/store.js
+++ src/store.js
@@ -1,23 +1,9 @@
-import { applyMiddleware, compose, createStore } from "redux";
-import thunkMiddleware from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 
-import monitorReducerEnhancer from "./enhancers/monitorReducer";
-import loggerMiddleware from "./middleware/logger";
 import rootReducer from "./reducers";
 
-const middlewareEnhancer = applyMiddleware(loggerMiddleware, thunkMiddleware);
-
-// 正式环境不记录 https://github.com/reduxjs/redux/issues/2359
-let composedEnhancers;
-if (process.env.NODE_ENV === "development") {
-  composedEnhancers = compose(
-    middlewareEnhancer,
-    monitorReducerEnhancer,
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-  );
-}
-
-const store = createStore(rootReducer, undefined, composedEnhancers);
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 export default store;
