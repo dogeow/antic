08688699b741cecf5e184ff1b21ebbcd88dc33e2
likunyan <lky216@gmail.com>
2020-10-22 23:47:46 +0800

    upgrade sentry

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import * as Sentry from "@sentry/browser";
+import * as Sentry from "@sentry/react";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
