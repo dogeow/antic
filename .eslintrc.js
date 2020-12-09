435644a42923dc4071ab914eca2a9fe2098d9c25
likunyan <lky216@gmail.com>
2020-12-09 23:18:18 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,8 @@ module.exports = {
   plugins: ["react", "prettier", "simple-import-sort"],
   rules: {
     "prettier/prettier": "error",
+    "require-jsdoc": 0,
+    "react/prop-types": 0,
     "simple-import-sort/imports": "error",
   },
 };
