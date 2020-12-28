f1c6e3cbb231c86c47928e181cf71e3c6977a0e8
likunyan <lky216@gmail.com>
2020-12-28 23:15:57 +0800

    Update PHPFunction.jsx

--- src/containers/PHPFunction.jsx
+++ src/containers/PHPFunction.jsx
@@ -4,18 +4,36 @@ import IconButton from "@material-ui/core/IconButton";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import InputLabel from "@material-ui/core/InputLabel";
 import OutlinedInput from "@material-ui/core/OutlinedInput";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import ClearIcon from "@material-ui/icons/Clear";
 import SearchIcon from "@material-ui/icons/Search";
 import axios from "axios";
 import React, { useState } from "react";
 
 const NoMatch = () => {
   const [value, setValue] = useState("");
-  const [phpFunction, setPhpFunction] = useState({});
+  const [phpFunction, setPhpFunction] = useState([]);
 
   const handleClickSearch = () => {
-    setPhpFunction({});
+    searchRequest();
+  };
+
+  const handleKeyUP = (e) => {
+    if (e.keyCode === 13) {
+      searchRequest();
+    }
+  };
+
+  const searchRequest = () => {
+    setPhpFunction([]);
     axios
-      .get("php-function", {
+      .post("php-function", {
         search: value,
       })
       .then((response) => {
@@ -31,8 +49,13 @@ const NoMatch = () => {
     setValue(e.target.value);
   };
 
+  const handleClear = () => {
+    setValue("");
+    setPhpFunction([]);
+  };
+
   return (
-    <>
+    <Grid>
       <Grid
         container
         justify="center"
@@ -49,38 +72,71 @@ const NoMatch = () => {
               type="text"
               value={value}
               onChange={handleChange}
+              onKeyUp={handleKeyUP}
               endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickSearch}
-                    onMouseDown={handleMouseDownSearch}
-                    edge="end"
-                  >
-                    <SearchIcon />
-                  </IconButton>
-                </InputAdornment>
+                <>
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="搜索"
+                      onClick={handleClear}
+                      onMouseDown={handleMouseDownSearch}
+                      edge="end"
+                    >
+                      <ClearIcon />
+                    </IconButton>
+                  </InputAdornment>
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="重置搜索"
+                      onClick={handleClickSearch}
+                      onMouseDown={handleMouseDownSearch}
+                      edge="end"
+                    >
+                      <SearchIcon />
+                    </IconButton>
+                  </InputAdornment>
+                </>
               }
               labelWidth={70}
             />
           </FormControl>
         </Grid>
       </Grid>
-      <Grid
-        container
-        justify="center"
-        justifyContent="center"
-        alignItems="center"
-        spacing={2}
-      >
-        {phpFunction && (
-          <>
-            <Grid item>{phpFunction.category}</Grid>
-            <Grid item>{phpFunction.intro}</Grid>
-          </>
-        )}
-      </Grid>
-    </>
+      {phpFunction.length > 0 && (
+        <Grid
+          container
+          justify="center"
+          justifyContent="center"
+          alignItems="center"
+          spacing={2}
+        >
+          <Grid item>
+            <TableContainer component={Paper}>
+              <Table aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>分类</TableCell>
+                    <TableCell align="right">函数名</TableCell>
+                    <TableCell align="right">简介</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {phpFunction.map((row) => (
+                    <TableRow key={row.name}>
+                      <TableCell component="th" scope="row">
+                        {row.category}
+                      </TableCell>
+                      <TableCell align="right">{row.name}</TableCell>
+                      <TableCell align="right">{row.intro}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+        </Grid>
+      )}
+    </Grid>
   );
 };
 
