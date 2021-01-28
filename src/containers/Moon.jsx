488023995c35b03261892182078e1763e825502d
likunyan <lky216@gmail.com>
2021-01-28 16:18:19 +0800

    233

--- src/containers/Moon.jsx
+++ src/containers/Moon.jsx
@@ -24,7 +24,7 @@ const Moon = () => {
       setMoonHistory(resp.data.history);
       setStatistics(resp.data.statistics);
     });
-  }, []);
+  }, [name]);
 
   const handleChange = (e) => {
     setName(e.target.value);
