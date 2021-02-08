717faa7075b39a8e98f6485b402dea7ed605f3a7
likunyan <lky216@gmail.com>
2021-02-09 01:14:16 +0800

    Update Chat.jsx

--- src/containers/Chat.jsx
+++ src/containers/Chat.jsx
@@ -1,16 +1,44 @@
 import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import InputBase from "@material-ui/core/InputBase";
+import Paper from "@material-ui/core/Paper";
 import Snackbar from "@material-ui/core/Snackbar";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import SearchIcon from "@material-ui/icons/Search";
 import axios from "axios";
 import React from "react";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: "2px 4px",
+    display: "flex",
+    alignItems: "center",
+    width: 400,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
+  },
+}));
+
 export default function Chat() {
-  const [content, setContent] = React.useState([]);
-  const [message, setMessage] = React.useState("");
+  const classes = useStyles();
+
+  const [chatBoard, setChatBoard] = React.useState([]);
+  const [content, setContent] = React.useState("");
   const [alertMessage, setAlertMessage] = React.useState("");
   const [open, setOpen] = React.useState(false);
 
   window.Echo.channel("chat").listen(".chat", (e) => {
-    setContent([...content, e.message]);
+    setChatBoard([...chatBoard, e.data]);
   });
 
   window.Echo.join("chat")
@@ -29,12 +57,12 @@ export default function Chat() {
 
   const socketId = window.Echo.socketId();
 
-  const handlePost = (e) => {
-    setContent([...content, message]);
+  const handlePost = () => {
+    setChatBoard([...chatBoard, content]);
     axios.post(
       "/chat",
       {
-        message: message,
+        message: content.message,
       },
       {
         headers: {
@@ -53,28 +81,48 @@ export default function Chat() {
   };
 
   const handleChange = (e) => {
-    const message = e.target.value;
-    setMessage(e.target.value);
+    setContent({
+      name: localStorage.userName,
+      message: e.target.value,
+    });
   };
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12}>
-        {content.length
-          ? content.map((item, index) => <div key={index}>{item}</div>)
-          : "无聊天内容"}
+    <Paper elevation={3} style={{ marginTop: 40 }}>
+      <Grid container style={{ padding: 20 }}>
+        <Grid item xs={12}>
+          {chatBoard.length
+            ? chatBoard.map((content, index) => (
+                <div key={index}>
+                  {content.name}: {content.message}
+                </div>
+              ))
+            : "无聊天内容"}
+        </Grid>
+        <Grid item xs={12}>
+          <InputBase
+            className={classes.input}
+            placeholder="输入聊天内容..."
+            inputProps={{ "aria-label": "输入聊天内容" }}
+            onChange={handleChange}
+          />
+          <IconButton
+            type="submit"
+            className={classes.iconButton}
+            aria-label="发送信息"
+            onClick={handlePost}
+          >
+            <SearchIcon />
+          </IconButton>
+        </Grid>
+        <Snackbar
+          open={open}
+          anchorOrigin={{ vertical: "top", horizontal: "center" }}
+          autoHideDuration={2000}
+          message={alertMessage}
+          onClose={handleClose}
+        />
       </Grid>
-      <Grid item xs={12}>
-        <input type="text" onChange={handleChange} />
-        <button onClick={handlePost}>发送</button>
-      </Grid>
-      <Snackbar
-        open={open}
-        anchorOrigin={{ vertical: "top", horizontal: "center" }}
-        autoHideDuration={2000}
-        message={alertMessage}
-        onClose={handleClose}
-      />
-    </Grid>
+    </Paper>
   );
 }
