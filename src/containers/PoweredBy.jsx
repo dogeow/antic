9df2dbd01811880cebb4045a78b4b837fb103691
likunyan <lky216@gmail.com>
2021-02-20 23:57:08 +0800

    Update PoweredBy.jsx

--- src/containers/PoweredBy.jsx
+++ src/containers/PoweredBy.jsx
@@ -4,67 +4,20 @@ export default function PowerBy() {
   return (
     <>
       <div>
-        <h2>前端</h2>
+        <h2>整体</h2>
         <ul>
-          <li>React(JavaScript</li>
-          <ul>
-            <li>Material-UI</li>
-            <ul>
-              <li>material design icons</li>
-              <li>pickers</li>
-              <li>@mdi/js</li>
-              <li>@mdi/react</li>
-            </ul>
-            <li>react-router-dom</li>
-            <li>Redux</li>
-            <ul>
-              <li>React Redux</li>
-              <li>Redux Toolkit</li>
-            </ul>
-            <li>SweetAlert 2</li>
-          </ul>
+          <li>前后端分离，使用 JWT</li>
+          <li>Material UI 风格</li>
           <li>GraphQL</li>
-          <ul>
-            <li>Apollo</li>
-          </ul>
-          <li>Axios</li>
-          <li>styled</li>
-          <li>loadable</li>
-          <li>imagesloaded</li>
-          <li>lodash</li>
-          <li>TOAST UI</li>
-          <li>Video.js</li>
-          <li>Day.js</li>
-          <li>socket.io</li>
-          <li>clipboard</li>
+          <li>WebSocket</li>
         </ul>
       </div>
-      <div>
-        <h2>后端</h2>
-        <ul>
-          <li>Laravel(PHP)</li>
-          <ul>
-            <li>jwt-auth</li>
-            <li>laravel-query-builder</li>
-            <li>Laravel-WebSockets</li>
-          </ul>
-        </ul>
-      </div>
-      <div>
-        <h2>服务器</h2>
-        <ul>
-          <li>Ubuntu 20.04 LTS</li>
-          <li>Nginx 1.14.0</li>
-          <li>MySQL 5.7</li>
-        </ul>
-      </div>
+      <h2>服务器：LNMP + Redis + ES</h2>
       <div>
         <h2>第三方</h2>
         <ul>
-          <li>Cloudflare</li>
-          <li>Algolia Search</li>
-          <li>OSS</li>
-          <li>Slack</li>
+          <li>OSS、短信</li>
+          <li>Slack Notification</li>
           <li>Sentry</li>
           <li>Gravatar</li>
         </ul>
