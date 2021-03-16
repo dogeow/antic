590de2b41cdb98752dbcf11653fdea0a1c7f09bb
likunyan <lky216@gmail.com>
2021-03-16 10:08:26 +0800

    Update Redirect.jsx

--- src/containers/Redirect.jsx
+++ src/containers/Redirect.jsx
@@ -3,8 +3,8 @@ import { toParams } from "../helpers";
 export default function () {
   const params = toParams(window.location.search.replace(/^\?/, ""));
   window.opener.postMessage(
-    { token: params.token },
-    "http://192.168.0.102:3000"
+    { code: params.code },
+    process.env.REACT_APP_DOMAIN
   );
   window.close();
 }
