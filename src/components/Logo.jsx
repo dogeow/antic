9c492a3d21549a0d7e3ce7635e0152c03d45353f
likunyan <lky216@gmail.com>
2021-03-18 13:25:16 +0800

    233

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,22 +1,6 @@
 import Avatar from "@material-ui/core/Avatar";
-import Grid from "@material-ui/core/Grid";
-import Hidden from "@material-ui/core/Hidden";
-import Typography from "@material-ui/core/Typography";
 import * as React from "react";
 
-const Logo = () => (
-  <Grid container alignItems="center">
-    <Grid item id="favicon">
-      <Avatar alt="Doge" src="/logo80.png" />
-    </Grid>
-    <Hidden only="xs">
-      <Grid item>
-        <Typography variant="h6" noWrap component="h1">
-          {process.env.REACT_APP_NAME}
-        </Typography>
-      </Grid>
-    </Hidden>
-  </Grid>
-);
+const Logo = () => <Avatar alt="Doge" src="/logo80.png" />;
 
 export default Logo;
