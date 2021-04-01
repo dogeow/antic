4c046fe6db7d94938067b35928c24e02b1492ed0
likunyan <lky216@gmail.com>
2021-04-01 23:23:00 +0800

    Update Index.jsx

--- src/containers/Index.jsx
+++ src/containers/Index.jsx
@@ -1,8 +1,23 @@
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
 import * as React from "react";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles(() => ({
+  "@global": {
+    a: {
+      display: "block",
+    },
+    "a:hover": {
+      backgroundColor: "rgba(255, 255, 255, 0.12)",
+    },
+  },
+}));
 
 const Index = () => {
+  useStyles();
+
   return (
     <Container maxWidth="xs">
       <Grid
@@ -12,26 +27,34 @@ const Index = () => {
         style={{ textAlign: "center" }}
       >
         <Grid item xs={6}>
-          <span role="img" style={{ fontSize: "3rem" }}>
-            📄
-          </span>
-          <div>笔记</div>
+          <Link to="/posts">
+            <span role="img" style={{ fontSize: "3rem" }}>
+              📄
+            </span>
+            <div>笔记</div>
+          </Link>
         </Grid>
         <Grid item xs={6}>
-          <span style={{ fontSize: "3rem" }}>♂</span>
-          <div>聊天室</div>
+          <Link to="/chat">
+            <span style={{ fontSize: "3rem" }}>♂</span>
+            <div>聊天室</div>
+          </Link>
         </Grid>
         <Grid item xs={6}>
-          <span role="img" style={{ fontSize: "3rem" }}>
-            ⚙️
-          </span>
-          <div>便民 API</div>
+          <Link to="/api">
+            <span role="img" style={{ fontSize: "3rem" }}>
+              ⚙️
+            </span>
+            <div>便民 API</div>
+          </Link>
         </Grid>
         <Grid item xs={6}>
-          <span role="img" style={{ fontSize: "3rem" }}>
-            📦
-          </span>
-          <div>Demo</div>
+          <Link to="/demo">
+            <span role="img" style={{ fontSize: "3rem" }}>
+              📦
+            </span>
+            <div>Demo</div>
+          </Link>
         </Grid>
       </Grid>
     </Container>
