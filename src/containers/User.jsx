a982793596e56bd9b81c469b692adc6689a02910
likunyan <lky216@gmail.com>
2021-04-17 18:09:37 +0800

    Update User.jsx

--- src/containers/User.jsx
+++ src/containers/User.jsx
@@ -3,30 +3,27 @@ import Grid from "@material-ui/core/Grid";
 import dayjs from "dayjs";
 import md5 from "md5";
 import * as React from "react";
-import { useStore } from "react-redux";
+import { useSelector } from "react-redux";
 
 import { gravatarCdn } from "../config/services";
 
 const User = () => {
-  const store = useStore();
-  const state = store.getState();
-  const avatar = `${gravatarCdn}/${md5(
-    state.lab.userEmail
-  )}.jpg?d=monsterid&s=300`;
+  const lab = useSelector((state) => state.lab);
+  const avatar = `${gravatarCdn}/${md5(lab.userEmail)}.jpg?d=monsterid&s=300`;
 
   return (
     <Grid container spacing={2} alignItems="center">
       <Grid item>
         <Avatar
-          alt={state.lab.user_name}
+          alt={lab.user_name}
           src={avatar}
           style={{ width: 150, height: 150 }}
         />
       </Grid>
       <Grid item>
-        <div>ID：{state.lab.userId}</div>
-        <div>昵称：{state.lab.userName}</div>
-        <div>邮箱：{state.lab.userEmail}</div>
+        <div>ID：{lab.userId}</div>
+        <div>昵称：{lab.userName}</div>
+        <div>邮箱：{lab.userEmail}</div>
       </Grid>
       <Grid item xs={12}>
         <ul>
