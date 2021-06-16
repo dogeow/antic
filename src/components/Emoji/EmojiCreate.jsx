9d8fd23db1bb0a26d98f9b26fb1f557582134bba
likunyan <lky216@gmail.com>
2021-06-16 16:14:12 +0800

    233

--- src/components/Emoji/EmojiCreate.jsx
+++ src/components/Emoji/EmojiCreate.jsx
@@ -1,71 +1,12 @@
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import React, { useCallback, useState } from "react";
-import { useDropzone } from "react-dropzone";
-import { useDispatch } from "react-redux";
+import React from "react";
 
-import { snackMessage } from "../../actions";
-import axios from "../../instance/axios";
+import Upload from "../../components/Upload";
 
 const EmojiCreate = () => {
-  const [data, setData] = useState();
-  const dispatch = useDispatch();
-
-  const onDrop = useCallback(
-    (acceptedFiles) => {
-      const file = new Blob([acceptedFiles[0]]);
-      const formData = new FormData();
-      formData.append("emoji", file, acceptedFiles[0].name);
-      axios
-        .post("/emoji", formData, {
-          headers: {
-            "Content-Type": "multipart/form-data",
-            Accept: "image/*",
-          },
-          transformRequest: [(data) => data],
-          onUploadProgress(e) {
-            const percentage = Math.round((e.loaded * 100) / e.total) || 0;
-            if (percentage < 100) {
-              window.console.log(`${percentage}%`); // 上传进度
-            }
-          },
-        })
-        .then(({ data }) => {
-          setData(data);
-          dispatch(snackMessage("上传成功"));
-        });
-    },
-    [dispatch]
-  );
-
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
-
   return (
     <Grid container spacing={2} justify="center" alignItems="flex-end">
-      <Grid item xs>
-        <Typography variant="body2" component="h3">
-          图片地址：{undefined === data ? "请先上传图片" : data.url}
-        </Typography>
-      </Grid>
-      <Grid
-        item
-        xs={12}
-        {...getRootProps()}
-        style={{
-          width: "100%",
-          border: "5px dotted",
-        }}
-      >
-        <input {...getInputProps()} />
-        {!data && isDragActive ? (
-          <p style={{ textAlign: "center" }}>拖放文件到这里</p>
-        ) : (
-          <p style={{ textAlign: "center" }}>拖放文件到这里，或点击选择文件</p>
-        )}
-        {data && (
-          <img src={data.url} alt="上传的图片" style={{ maxWidth: "100%" }} />
-        )}
-      </Grid>
+      <Upload path="/emoji" />
     </Grid>
   );
 };
---
+++ src/containers/goods/Index.jsx
@@ -0,0 +1,40 @@
+import Grid from "@material-ui/core/Grid";
+import React from "react";
+
+import Upload from "../../components/Upload";
+
+const EmojiCreate = () => {
+  return (
+    <Grid container spacing={2} justify="center" alignItems="flex-end">
+      <Upload path="/emoji" />
+      <Grid item xs={12}>
+        更多照片？
+      </Grid>
+      <Grid container justify="space-between">
+        <Grid item>分类</Grid>
+        <Grid item>分类</Grid>
+      </Grid>
+      <Grid item xs={12}>
+        标题
+      </Grid>
+      <Grid item xs={12}>
+        <h3>草稿</h3>
+        <ul>
+          <li>
+            随着生活水平提高，闲置东西越来越多，有时候我们可以用不玩的 Switch 换
+            GoPro，或者互相借用。但是，这个系统不是全部公开的，默认是仅限于自己和自己的朋友或亲戚之间。因为，咸鱼那些交易比较麻烦，东西不值钱的，也没必要浪费时间去弄，比如
+            VGA 线自己不再使用了，但是可以放着，朋友需要就可以拿去。
+          </li>
+          <li>
+            一：管理自己的物品。二：可以设置为免费或者物物交换、可互相借用
+          </li>
+          <li>一般都是手机操作，界面和操作要友好</li>
+          <li>一键查看自己需要的东西</li>
+          <li>属性可以自己添加</li>
+        </ul>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default EmojiCreate;
