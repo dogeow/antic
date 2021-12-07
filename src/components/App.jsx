b15b24cf58b36a519490f6d5bde4ea65910287cc
likunyan <lky216@gmail.com>
2021-12-07 12:45:03 +0800

    233

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
+import "dayjs/locale/zh-cn";
+
 import AdapterDayjs from "@mui/lab/AdapterDayjs";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import CssBaseline from "@mui/material/CssBaseline";
 import { StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
 import themeCustomization from "config/theme";
+import dayjs from "dayjs";
 import PropTypes from "prop-types";
 import * as React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "routes";
 
 import ScrollToTop from "./ScrollToTop";
+
+dayjs.locale("zh-cn");
 
 /**
  *
