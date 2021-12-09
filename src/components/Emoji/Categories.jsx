8d34f3d6dd078eaf66ad34b0ec03469eb6a6285e
likunyan <lky216@gmail.com>
2021-12-09 17:06:53 +0800

    Revert "Update Categories.jsx"
    This reverts commit 81b36a31ac453af9cdcd3986aa060c16d8a0ffa1.

--- src/components/Emoji/Categories.jsx
+++ src/components/Emoji/Categories.jsx
@@ -1,24 +1,22 @@
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import * as React from "react";
-import { useSelector } from "react-redux";
 
 import CategoriesElem from "./CategoriesElem";
 
 const Category = (props) => {
-  const expandCategory = useSelector((state) => state.emoji.expandCategory);
   const color = props.selectedCategory === "全部" ? "secondary" : "primary";
 
   return (
     <Grid container alignItems="center">
       <Grid item>
         <Button variant="contained" onClick={() => props.toggleCategory()}>
-          {expandCategory ? "分类 <<" : "分类 >>"}
+          {props.expandCategory ? "分类 <<" : "分类 >>"}
         </Button>
       </Grid>
       ：
       <Grid item>
-        {expandCategory ? (
+        {props.expandCategory ? (
           <Button
             variant={
               props.lab.paletteMode === "dark" ? "outlined" : "contained"
@@ -32,7 +30,7 @@ const Category = (props) => {
           props.selectedCategory
         )}
       </Grid>
-      {expandCategory && <CategoriesElem {...props} />}
+      {props.expandCategory && <CategoriesElem {...props} />}
     </Grid>
   );
 };
