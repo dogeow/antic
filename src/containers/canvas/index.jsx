ea0337d5ae6c0883a9e2964604bf470e0d25ca79
likunyan <lky216@gmail.com>
2021-12-13 16:17:22 +0800

    233

--- src/containers/canvas/index.jsx
+++ src/containers/canvas/index.jsx
@@ -5,9 +5,17 @@ import axios from "instance/axios";
 import React, { useCallback, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
+import {
+  LAYERS,
+  MAP_DIMENSIONS,
+  MAP_TILE_IMAGES,
+  TILE_SIZE,
+} from "./constants";
+
 export default () => {
   const [users, setUsers] = useState([]);
   const token = useSelector((state) => state.lab.token);
+  const { COLS, ROWS } = MAP_DIMENSIONS;
 
   const game = useCallback((e) => {
     const x = e.pageX;
@@ -41,13 +49,7 @@ export default () => {
     );
   }, []);
 
-  useEffect(() => {
-    // 监听事件
-    document.addEventListener("mousedown", game);
-
-    return () => document.removeEventListener("mousedown", game);
-  }, [game]);
-
+  // WebSocket
   useEffect(() => {
     window.Echo.options.auth.headers.Authorization = token;
 
@@ -70,18 +72,86 @@ export default () => {
     };
   }, [token]);
 
+  // 点击事件
+  useEffect(() => {
+    // 监听事件
+    document.addEventListener("mousedown", game);
+
+    return () => document.removeEventListener("mousedown", game);
+  }, [game]);
+
   const draw = (ctx, frameCount) => {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
+    const drawLayer = (grid) => {
+      for (let i = 0; i < ROWS; i++) {
+        for (let j = 0; j < COLS; j++) {
+          const item = grid[i][j];
+          if (!item) {
+            // empty tile
+            continue;
+          }
+          const img = document.querySelector(`#map-tile-img-${item}`);
+          const x = j * TILE_SIZE;
+          const y = i * TILE_SIZE;
+          ctx.drawImage(
+            img,
+            0,
+            0,
+            TILE_SIZE,
+            TILE_SIZE,
+            x,
+            y,
+            TILE_SIZE,
+            TILE_SIZE
+          );
+        }
+      }
+    };
+
+    drawLayer(LAYERS[0]);
+    drawLayer(LAYERS[1]);
+
     ctx.save();
     for (const user of users) {
-      ctx.font = "24px JetBrains Mono, monospace";
-      ctx.fillText(user.name, user.loc.x, user.loc.y);
+      // ctx.font = "24px JetBrains Mono, monospace";
+      // ctx.fillText(user.name, user.loc.x, user.loc.y);
+      ctx.drawImage(
+        document.querySelector("#character"),
+        0,
+        0,
+        32 - 5,
+        32 - 5,
+        user.loc.x,
+        user.loc.y,
+        32,
+        32
+      );
     }
     ctx.restore();
   };
 
   const canvasRef = useCanvas(draw);
 
-  return <canvas id="canvas" ref={canvasRef} width="800px" height="400px" />;
+  return (
+    <>
+      <div className="images-buffer" style={{ display: "none" }}>
+        {Object.keys(MAP_TILE_IMAGES).map((key) => (
+          <img
+            key={`map-tile-img-${key}`}
+            id={`map-tile-img-${key}`}
+            src={`${MAP_TILE_IMAGES[key]}`}
+            alt={`map-tile-${key}`}
+          />
+        ))}
+        <img
+          id="character"
+          alt="character"
+          className="images-buffer"
+          src="assets/heroes/heroes.png"
+        />
+      </div>
+      <canvas id="canvas" ref={canvasRef} width="800px" height="400px" />
+    </>
+  );
 };
