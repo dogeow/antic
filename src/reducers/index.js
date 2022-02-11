1fb703d5b7c07160d4f9642f0c0fa45a169fa9a5
likunyan <lky216@gmail.com>
2022-02-11 17:58:01 +0800

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,9 @@ import emoji from "./emoji";
 import lab from "./lab";
 import post from "./post";
 
-export default (state = {}, action) => {
-  return {
-    lab: lab(state.lab, action), // 实验室，学习 React
-    post: post(state.post, action), // 文章
-    chat: chat(state.chat, action), // 聊天室
-    emoji: emoji(state.emoji, action), // 表情包
-  };
-};
+export default (state = {}, action) => ({
+  lab: lab(state.lab, action), // 实验室，学习 React
+  post: post(state.post, action), // 文章
+  chat: chat(state.chat, action), // 聊天室
+  emoji: emoji(state.emoji, action), // 表情包
+});
