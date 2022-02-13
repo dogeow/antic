5bb9ad45ebd98a72f69ddcaa7a4fd869b225fed6
likunyan <lky216@gmail.com>
2022-02-13 22:42:50 +0800

    Update bootstrap.js

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -42,67 +42,66 @@ window.Echo = new Echo({
   },
 });
 
-// 增加 JavaScript 没有的函数（PHP 上的）
-((ns) => {
-  /**
-   * mbStrWidth
-   * @param {string} str
-   * @return {int}
-   * @see http://php.net/manual/ja/function.mb-strwidth.php
-   */
-  const mbStrWidth = (str) => {
-    const l = str.length;
-    let c = "";
-    let length = 0;
-    for (let i = 0; i < l; i++) {
-      c = str.charCodeAt(i);
-      if (c >= 0x0000 && c <= 0x0019) {
-        length += 0;
-      } else if (c >= 0x0020 && c <= 0x1fff) {
-        length += 1;
-      } else if (c >= 0x2000 && c <= 0xff60) {
-        length += 2;
-      } else if (c >= 0xff61 && c <= 0xff9f) {
-        length += 1;
-      } else if (c >= 0xffa0) {
-        length += 2;
-      }
+/**
+ * mbStrWidth
+ * @param {string} str
+ * @return {int}
+ * @see http://php.net/manual/ja/function.mb-strwidth.php
+ */
+// eslint-disable-next-line no-extend-native
+String.prototype.mbStrWidth = (str) => {
+  const l = str.length;
+  let c = "";
+  let length = 0;
+  for (let i = 0; i < l; i++) {
+    c = str.charCodeAt(i);
+    if (c >= 0x0000 && c <= 0x0019) {
+      length += 0;
+    } else if (c >= 0x0020 && c <= 0x1fff) {
+      length += 1;
+    } else if (c >= 0x2000 && c <= 0xff60) {
+      length += 2;
+    } else if (c >= 0xff61 && c <= 0xff9f) {
+      length += 1;
+    } else if (c >= 0xffa0) {
+      length += 2;
     }
-    return length;
-  };
+  }
 
-  /**
-   * mbStrImWidth
-   * @param {string} str
-   * @param {int} start
-   * @param {int}  width
-   * @param {string} trimMarker
-   * @return {string}
-   * @see http://www.php.net/manual/ja/function.mb-strimwidth.php
-   */
-  const mbStrImWidth = (str, start, width, trimMarker = "") => {
-    const trimMakerWidth = mbStrWidth(trimMarker);
-    const l = str.length;
-    let trimmedLength = 0;
-    let trimmedStr = "";
-    for (let i = start; i < l; i++) {
-      // const charCode = str.charCodeAt(i);
-      const c = str.charAt(i);
-      const charWidth = mbStrWidth(c);
-      const next = str.charAt(i + 1);
-      const nextWidth = mbStrWidth(next);
-      trimmedLength += charWidth;
-      trimmedStr += c;
-      if (trimmedLength + trimMakerWidth + nextWidth > width) {
-        trimmedStr += trimMarker;
-        break;
-      }
+  return length;
+};
+
+/**
+ * mbStrImWidth
+ * @param {string} str
+ * @param {int} start
+ * @param {int}  width
+ * @param {string} trimMarker
+ * @return {string}
+ * @see http://www.php.net/manual/ja/function.mb-strimwidth.php
+ */
+// eslint-disable-next-line no-extend-native
+String.prototype.mbStrImWidth = (str, start, width, trimMarker = "") => {
+  const trimMakerWidth = trimMarker.mbStrWidth;
+  const l = str.length;
+  let trimmedLength = 0;
+  let trimmedStr = "";
+  for (let i = start; i < l; i++) {
+    // const charCode = str.charCodeAt(i);
+    const c = str.charAt(i);
+    const charWidth = c.mbStrWidth;
+    const next = str.charAt(i + 1);
+    const nextWidth = next.mbStrWidth;
+    trimmedLength += charWidth;
+    trimmedStr += c;
+    if (trimmedLength + trimMakerWidth + nextWidth > width) {
+      trimmedStr += trimMarker;
+      break;
     }
-    return trimmedStr;
-  };
-  ns.mbStrWidth = mbStrWidth;
-  ns.mbStrImWidth = mbStrImWidth;
-})(window);
+  }
+
+  return trimmedStr;
+};
 
 // eslint-disable-next-line no-extend-native
 String.prototype.colorHex = function () {
