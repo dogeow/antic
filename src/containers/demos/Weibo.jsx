8153d18b2e9c5690ba416e585c955b433b93835e
likunyan <lky216@gmail.com>
2022-05-07 12:29:38 +0800

    Update Weibo.jsx

--- src/containers/demos/Weibo.jsx
+++ src/containers/demos/Weibo.jsx
@@ -8,6 +8,7 @@ import {
   Skeleton,
   TextField,
 } from "@mui/material";
+import Alert from "@mui/material/Alert";
 import makeStyles from "@mui/styles/makeStyles";
 import dayjs from "dayjs";
 import axios from "instance/axios";
@@ -37,7 +38,7 @@ const Weibo = () => {
   useEffect(() => {
     const selectDate = dayjs(selectedDate).format("YYYY-MM-DD");
     axios
-      .post(`weibo?date=${selectDate}&page[number]=${currPage}`)
+      .get(`weibo?date=${selectDate}&page[number]=${currPage}`)
       .then(({ data }) => {
         setWeibo(data);
         setCurrPage(data.current_page);
@@ -60,87 +61,94 @@ const Weibo = () => {
   };
 
   return (
-    <Grid container spacing={2} justifyContent="center">
-      <Grid item xs={12}>
-        <DatePicker
-          label="日期"
-          format="YYYY-MM-DD"
-          disableFuture
-          value={selectedDate}
-          onChange={handleDateChange}
-          renderInput={(props) => <TextField {...props} />}
-        />
-        <div style={{ float: "right", marginRight: 12 }}>
-          <Link to="/weibo/about">
-            <ErrorOutline />
-          </Link>
-        </div>
-      </Grid>
-      <Grid item xs={12}>
-        <table style={{ width: "100%" }}>
-          <thead>
-            <tr>
-              <th style={{ textAlign: "left" }}>No.</th>
-              <th style={{ textAlign: "left" }}>话题</th>
-              <Hidden only="xs">
-                <th>热度</th>
-                <th style={{ textAlign: "right" }}>更新于</th>
-              </Hidden>
-            </tr>
-          </thead>
-          <tbody>
-            {weibo.data
-              ? weibo.data.map((item, index) => (
-                  <tr key={item.id} className={item.status}>
-                    <td>{weibo.per_page * (currPage - 1) + index + 1}</td>
-                    <td>
-                      <span style={{ float: "left" }}>
-                        <a href={`https://s.weibo.com${item.url}`}>
-                          {item.title}
-                        </a>
-                      </span>
-                      {item.emoji && (
-                        <span
-                          dangerouslySetInnerHTML={{ __html: item.emoji }}
+    <>
+      <Alert severity="warning" style={{ marginBottom: 20 }}>
+        该功能已停止更新，只有旧数据。
+      </Alert>
+      <Grid container spacing={2} justifyContent="center">
+        <Grid item xs={12}>
+          <DatePicker
+            label="日期"
+            format="YYYY-MM-DD"
+            disableFuture
+            value={selectedDate}
+            onChange={handleDateChange}
+            renderInput={(props) => <TextField {...props} />}
+          />
+          <div style={{ float: "right", marginRight: 12 }}>
+            <Link to="/weibo/about">
+              <ErrorOutline />
+            </Link>
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <table style={{ width: "100%" }}>
+            <thead>
+              <tr>
+                <th style={{ textAlign: "left" }}>No.</th>
+                <th style={{ textAlign: "left" }}>话题</th>
+                <Hidden only="xs">
+                  <th>热度</th>
+                  <th style={{ textAlign: "right" }}>更新于</th>
+                </Hidden>
+              </tr>
+            </thead>
+            <tbody>
+              {weibo.data
+                ? weibo.data.map((item, index) => (
+                    <tr key={item.id} className={item.status}>
+                      <td>{weibo.per_page * (currPage - 1) + index + 1}</td>
+                      <td>
+                        <span style={{ float: "left" }}>
+                          <a href={`https://s.weibo.com${item.url}`}>
+                            {item.title}
+                          </a>
+                        </span>
+                        {item.emoji && (
+                          <span
+                            dangerouslySetInnerHTML={{ __html: item.emoji }}
+                          />
+                        )}
+                      </td>
+                      <Hidden only="xs">
+                        <td>{item.rank}</td>
+                        <td style={{ textAlign: "right" }}>
+                          {item.updated_at}
+                        </td>
+                      </Hidden>
+                    </tr>
+                  ))
+                : Array.from(new Array(20)).map((item, index) => (
+                    <tr key={index}>
+                      <td colSpan={2}>
+                        <Skeleton
+                          variant="rectangular"
+                          width={`${random(20, 88)}%`}
+                          height={20}
                         />
-                      )}
-                    </td>
-                    <Hidden only="xs">
-                      <td>{item.rank}</td>
-                      <td style={{ textAlign: "right" }}>{item.updated_at}</td>
-                    </Hidden>
-                  </tr>
-                ))
-              : Array.from(new Array(20)).map((item, index) => (
-                  <tr key={index}>
-                    <td colSpan={2}>
-                      <Skeleton
-                        variant="rectangular"
-                        width={`${random(20, 88)}%`}
-                        height={20}
-                      />
-                    </td>
-                  </tr>
-                ))}
-          </tbody>
-        </table>
+                      </td>
+                    </tr>
+                  ))}
+            </tbody>
+          </table>
+        </Grid>
+        <Grid item xs={12}>
+          <Pagination
+            page={currPage}
+            count={pageCount}
+            hidePrevButton={currPage <= 1}
+            hideNextButton={currPage >= weibo.last_page}
+            renderItem={(item) => (
+              <PaginationItem
+                {...item}
+                disabled={item.page === currPage}
+                onClick={() => handlePage(item.page)}
+              />
+            )}
+          />
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <Pagination
-          page={currPage}
-          count={pageCount}
-          hidePrevButton={currPage <= 1}
-          hideNextButton={currPage >= weibo.last_page}
-          renderItem={(item) => (
-            <PaginationItem
-              {...item}
-              disabled={item.page === currPage}
-              onClick={() => handlePage(item.page)}
-            />
-          )}
-        />
-      </Grid>
-    </Grid>
+    </>
   );
 };
 
