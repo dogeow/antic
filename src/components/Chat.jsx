5e0c39360617eb0cd81a452aa95df08cd8ffc843
DogeOW <lky216@gmail.com>
2022-08-02 23:57:09 +0800

    Update Chat.jsx

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -72,6 +72,8 @@ export default function Chat() {
 
     let typingTime;
 
+    window.Echo.options.auth.headers.Authorization = user.token;
+
     window.Echo.join("chat")
       .here((user) => {
         setLoading(false);
