699cf66526a5bf5f5d71e88dc92a04eb688186d6
DogeOW <lky216@gmail.com>
2022-10-20 18:44:27 +0800

    Update Tags.tsx

--- src/containers/post/Tags.tsx
+++ src/containers/post/Tags.tsx
@@ -6,7 +6,7 @@ import makeStyles from "@mui/styles/makeStyles";
 import React, { useEffect } from "react";
 import { useRecoilState } from "recoil";
 
-import { TAGS_WITH_COUNT } from "../../graphql/post";
+import { TAGS } from "../../graphql/post";
 
 const useStyles = makeStyles((theme: Theme) => ({
   tags: {
@@ -27,7 +27,7 @@ const Tags = ({ edit }) => {
   const [post] = useRecoilState<Post>(postState);
   const [user] = useRecoilState(userState);
 
-  const { data } = useQuery(TAGS_WITH_COUNT);
+  const { data } = useQuery(TAGS);
 
   useEffect(() => {
     if (data) {
