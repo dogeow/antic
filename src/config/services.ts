c642f2cca44873306c709c5dc2381ca110ee1c6c
example <example@example.com>
2022-12-02 10:23:58 +0800

    Update services.ts

--- src/config/services.ts
+++ src/config/services.ts
@@ -1,4 +1,4 @@
-export const GRAVATAR_CDN = "https://gravatar.loli.net/avatar";
+export const GRAVATAR_CDN = "https://sdn.geekzu.org/avatar";
 export const CDN_URL = "https://upyun.dogeow.com";
 export const OSS_URL = "https://oss.dogeow.com";
 export const QUOTE_FONT = "https://fonts.googleapis.com/css2?family=Long+Cang&display=swap";
