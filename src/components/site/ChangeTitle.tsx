a9507f065d4269cbbd411c522ea3dc33ea83d537
example <example@example.com>
2023-01-17 14:38:26 +0800

    Update ChangeTitle.tsx

--- src/components/site/ChangeTitle.tsx
+++ src/components/site/ChangeTitle.tsx
@@ -1,27 +1,11 @@
 export default function (leave: string, come: string) {
-  // 网页当前状态判断
-  let state: string;
-  let visibilityChange;
   if (typeof document.hidden !== "undefined") {
-    visibilityChange = "visibilitychange";
-    state = "visibilityState";
-  } else if (typeof document.mozHidden !== "undefined") {
-    visibilityChange = "mozvisibilitychange";
-    state = "mozVisibilityState";
-  } else if (typeof document.msHidden !== "undefined") {
-    visibilityChange = "msvisibilitychange";
-    state = "msVisibilityState";
-  } else if (typeof document.webkitHidden !== "undefined") {
-    visibilityChange = "webkitvisibilitychange";
-    state = "webkitVisibilityState";
+    document.addEventListener(
+      "visibilitychange",
+      () => {
+        document.title = document.visibilityState === "hidden" ? leave : come;
+      },
+      false
+    );
   }
-
-  // 添加监听器，在 title 里显示状态变化
-  document.addEventListener(
-    visibilityChange,
-    () => {
-      document.title = document[state] === "hidden" ? leave : come;
-    },
-    false
-  );
 }
