5464473d6ddcc8b89804209ca9bfaa65e4163edd
likunyan <lky216@gmail.com>
2023-03-13 10:31:15 +0800

    233

--- src/config/services.ts
+++ src/config/services.ts
@@ -1,4 +1,4 @@
-export const GRAVATAR_CDN = "https://sdn.geekzu.org/avatar";
+export const GRAVATAR_CDN = "https://gravatar.ityinhu.com/avatar"; // cravatar.cn 备用
 export const CDN_URL = "https://upyun.dogeow.com";
 export const OSS_URL = "https://oss.dogeow.com";
 export const QUOTE_FONT = "https://fonts.googleapis.com/css2?family=Long+Cang&display=swap";
--- src/containers/tools/Ai.tsx
+++ src/containers/tools/Ai.tsx
@@ -1,5 +1,6 @@
 import MuiAlert from "@mui/material/Alert";
 import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
 import Grid from "@mui/material/Grid";
 import Snackbar from "@mui/material/Snackbar";
 import TextField from "@mui/material/TextField";
@@ -9,7 +10,6 @@ import * as React from "react";
 import { useState } from "react";
 
 import ClipboardButton from "../../components/ClipboardButton";
-import axios from "../../instance/axios";
 
 const useStyles = makeStyles((theme) => ({
   response: {
@@ -25,11 +25,24 @@ const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
+function processMatch(jsonObj) {
+  const { choices } = jsonObj;
+  const { delta } = choices[0];
+
+  if (delta.content) {
+    return delta.content;
+  }
+  return "";
+}
+
 const Ai = () => {
   const classes = useStyles();
   const [content, setContent] = React.useState("");
   const [response, setResponse] = React.useState("");
   const [open, setOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [responseChunks, setResponseChunks] = useState([]);
 
   const getClipboardButton = (value) => {
     return (
@@ -53,43 +66,114 @@ const Ai = () => {
   };
 
   const handleInputValueChange = (event) => {
+    if (errorMessage !== "") {
+      setErrorMessage("");
+    }
     setContent(event.target.value);
-    console.log(event.target.value);
   };
 
-  const handleSendButtonClick = () => {
-    axios
-      .post("/ai", { content })
-      .then((response) => {
-        setResponse(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setIsLoading(true);
+
+    try {
+      const response = await fetch("https://mirror.dogeow.com/ai", {
+        method: "POST",
+        body: JSON.stringify({ content }),
+        headers: {
+          "Content-Type": "application/json",
+        },
       });
+
+      if (response.ok) {
+        const reader = response.body.getReader();
+
+        let remainingBytes = new Uint8Array();
+
+        const decoder = new TextDecoder();
+
+        while (true) {
+          console.log("----");
+          const chunks = [];
+          const { done, value } = await reader.read();
+          if (done) break;
+
+          const dataBytes = new Uint8Array([...remainingBytes, ...value]);
+          const decodedText = decoder.decode(dataBytes, { stream: true });
+
+          const regex = /data:\s*({.*]})/gm;
+          let match;
+          const matches = [];
+
+          while ((match = regex.exec(decodedText))) {
+            const jsonStr = match[1];
+            const jsonObj = JSON.parse(jsonStr);
+            matches.push(jsonObj);
+          }
+          const numMatches = matches.length;
+          const lastIndex = regex.lastIndex;
+          if (numMatches < decodedText.length - 1) {
+            remainingBytes = dataBytes.slice(lastIndex);
+          } else {
+            remainingBytes = new Uint8Array();
+          }
+          // 处理所有匹配
+          matches.forEach((match) => {
+            const content = processMatch(match);
+            chunks.push(content);
+            setResponseChunks(chunks); // 更新响应数据列表
+          });
+        }
+
+        console.log("Data Stream Complete!");
+      } else {
+        console.error(`HTTP Error ${response.status}: ${response.statusText}`);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
   };
+
+  // 将所有数据块合并成一个字符串
+  const responseString = responseChunks.join("");
 
   return (
     <Grid container spacing={1} maxWidth={"sm"} minWidth={"xs"} alignItems={"center"} justifyContent={"center"}>
       <Grid item xs={12}>
         <TextField
-          multiline
-          rows={3}
           label="问题"
-          variant="outlined"
+          required
           value={content}
           fullWidth
-          placeholder={"请输入问题"}
+          variant="outlined"
+          rows={3}
+          multiline
+          autoFocus={true}
+          placeholder={"请输入问题，最短2个字，最多255个字。"}
           onChange={handleInputValueChange}
+          helperText={errorMessage}
+          InputLabelProps={errorMessage !== "" ? { shrink: true } : {}}
+          error={errorMessage !== ""}
         />
       </Grid>
       <Grid item xs={12}>
-        <Button variant="contained" color="primary" onClick={handleSendButtonClick} fullWidth>
-          发送
+        <Button
+          variant="contained"
+          color={isLoading ? undefined : "primary"}
+          disabled={isLoading}
+          onClick={handleSubmit}
+          fullWidth
+          startIcon={isLoading ? <CircularProgress size={20} color="inherit" /> : null}
+        >
+          {isLoading ? "请等待..." : "发送"}
         </Button>
       </Grid>
       <Grid item xs={12}>
-        <Typography className={classes.response} variant="body1" style={{ padding: 14 }}>
-          {response}
+        <Typography className={classes.response} variant="body1" style={{ padding: 14, whiteSpace: "pre-wrap" }}>
+          {responseString}
         </Typography>
       </Grid>
       <Grid item xs={12}>
