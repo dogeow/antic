1c08bef743955dc0952d09950b9256fab30a2776
likunyan <lky216@gmail.com>
2023-03-24 17:22:56 +0800

    233

--- src/projects/things/Thing.tsx
+++ src/projects/things/Thing.tsx
@@ -51,54 +51,58 @@ function Thing() {
 
   return (
     <Grid container>
-      <Collapse accordion style={{ flexGrow: 1 }}>
-        {things.map((thing) => (
-          <Collapse.Panel
-            key={thing.id}
-            title={
-              <div style={{ display: "flex" }}>
-                <Image
-                  lazy
-                  src={thing.photos[0].path}
-                  width={50}
-                  height={50}
-                  fit="cover"
-                  key={thing.photos[0].id}
-                  style={{ borderRadius: 4 }}
-                />
-                <div style={{ marginLeft: 4 }}>
-                  <h2>{thing.name}</h2>
-                  <div>
-                    {thing.tags.map((tag) => (
-                      <Tag round color="#2db7f5" className="tag" key={tag.id}>
-                        {tag.name}
-                      </Tag>
-                    ))}
+      <Grid item xs={12}>
+        <Collapse accordion>
+          {things.map((thing) => (
+            <Collapse.Panel
+              key={thing.id}
+              title={
+                <div style={{ display: "flex" }}>
+                  <Image
+                    lazy
+                    src={thing.photos[0].path}
+                    width={50}
+                    height={50}
+                    fit="cover"
+                    key={thing.photos[0].id}
+                    style={{ borderRadius: 4 }}
+                  />
+                  <div style={{ marginLeft: 4 }}>
+                    <h2 style={{ fontSize: "1rem" }}>{thing.name}</h2>
+                    <div>
+                      {thing.tags.map((tag) => (
+                        <Tag round color="#2db7f5" className="tag" key={tag.id}>
+                          {tag.name}
+                        </Tag>
+                      ))}
+                    </div>
                   </div>
                 </div>
-              </div>
-            }
-          >
-            <p style={{ marginBottom: 6 }}>{thing.description}</p>
-            <Space wrap>
-              {thing.photos.map((photo) => (
-                <Image
-                  lazy
-                  src={photo.path}
-                  width={100}
-                  height={100}
-                  fit="cover"
-                  key={photo.id}
-                  style={{ borderRadius: 4 }}
-                />
-              ))}
-            </Space>
-          </Collapse.Panel>
-        ))}
-      </Collapse>
-      <InfiniteScroll loadMore={loadMore} hasMore={hasMore}>
-        <InfiniteScrollContent hasMore={hasMore} />
-      </InfiniteScroll>
+              }
+            >
+              <p style={{ marginBottom: 6 }}>{thing.description}</p>
+              <Space wrap>
+                {thing.photos.map((photo) => (
+                  <Image
+                    lazy
+                    src={photo.path}
+                    width={100}
+                    height={100}
+                    fit="cover"
+                    key={photo.id}
+                    style={{ borderRadius: 4 }}
+                  />
+                ))}
+              </Space>
+            </Collapse.Panel>
+          ))}
+        </Collapse>
+      </Grid>
+      <Grid item xs={12}>
+        <InfiniteScroll loadMore={loadMore} hasMore={hasMore}>
+          <InfiniteScrollContent hasMore={hasMore} />
+        </InfiniteScroll>
+      </Grid>
       <div className="bottom">
         <Bottom />
       </div>
