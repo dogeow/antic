2ebb074266ebd330f0fdacfbbe7c5488a479acfc
likunyan <lky216@gmail.com>
2023-03-31 20:01:13 +0800

    Update index.tsx

--- src/containers/nav/index.tsx
+++ src/containers/nav/index.tsx
@@ -1,15 +1,28 @@
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
 import React from "react";
 import { isMobile } from "react-device-detect";
 
 import NavForMobile from "./NavForMobile";
+import NavForMobile2 from "./NavForMobile2";
 import NavForPc from "./NavForPc";
 
 const Index = () => {
-  if (isMobile) {
-    return <NavForMobile />;
-  } else {
-    return <NavForPc />;
-  }
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+
+  return (
+    <>
+      <FormControlLabel
+        control={<Switch checked={checked} onChange={handleChange} inputProps={{ "aria-label": "controlled" }} />}
+        label="树形显示"
+      />
+      {isMobile ? checked ? <NavForMobile2 /> : <NavForMobile /> : <NavForPc />}
+    </>
+  );
 };
 
 export default Index;
