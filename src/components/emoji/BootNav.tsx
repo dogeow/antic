058400e5a5b145cf0d3782afda9a5e0ad29c53ad
likunyan <lky216@gmail.com>
2023-05-26 11:17:27 +0800

    Update BootNav.tsx

--- src/components/emoji/BootNav.tsx
+++ src/components/emoji/BootNav.tsx
@@ -9,48 +9,57 @@ import Swal from "sweetalert2";
 
 import { currentPageState, faceIsLoadingState, filteredEmojiListState, pageLimitState } from "../../states";
 
+const PREVIOUS = "Previous";
+const NEXT = "Next";
+
+const showWarningMessage = (title: string) => {
+  Swal.fire({
+    position: "center",
+    type: "info",
+    title: title,
+    showConfirmButton: false,
+    timer: 1000,
+  });
+};
+
 const BootNav = () => {
   const filteredFaces = useRecoilValue(filteredEmojiListState);
   const [currentPage, setCurrentPage] = useRecoilState(currentPageState);
   const [pageLimit] = useRecoilState(pageLimitState);
   const [, setFaceIsLoading] = useRecoilState(faceIsLoadingState);
 
-  const handlePageSwitch = (action: "Next" | "Previous") => {
-    if (action === "Previous") {
+  const handlePageSwitch = (action: typeof PREVIOUS | typeof NEXT) => {
+    let pageChangeSuccessful = false;
+
+    if (action === PREVIOUS) {
       if (currentPage <= 1) {
-        Swal.fire({
-          position: "center",
-          type: "info",
-          title: "最前了，没有上一页！",
-          showConfirmButton: false,
-          timer: 1000,
-        });
-        return;
+        showWarningMessage("最前了，没有上一页！");
+      } else {
+        setFaceIsLoading(true);
+        setCurrentPage(currentPage - 1);
+        pageChangeSuccessful = true;
       }
-      setFaceIsLoading(true);
-      setCurrentPage(currentPage - 1);
     }
-    if (action === "Next") {
+
+    if (action === NEXT) {
       if (currentPage >= Math.ceil(filteredFaces.length / pageLimit)) {
-        Swal.fire({
-          position: "center",
-          type: "info",
-          title: "最后了，没有下一页！",
-          showConfirmButton: false,
-          timer: 1000,
-        });
+        showWarningMessage("最后了，没有下一页！");
       } else {
         setFaceIsLoading(true);
         setCurrentPage(currentPage + 1);
+        pageChangeSuccessful = true;
       }
     }
-    scrollToElement("#emoji");
+
+    if (pageChangeSuccessful) {
+      scrollToElement("#emoji");
+    }
   };
 
   return (
     <BottomNavigation showLabels value={-1} style={{ right: 0, left: 0, bottom: 0, position: "fixed" }}>
-      <BottomNavigationAction label="上页" icon={<FirstPage />} onClick={() => handlePageSwitch("Previous")} />
-      <BottomNavigationAction label="下页" icon={<LastPage />} onClick={() => handlePageSwitch("Next")} />
+      <BottomNavigationAction label="上页" icon={<FirstPage />} onClick={() => handlePageSwitch(PREVIOUS)} />
+      <BottomNavigationAction label="下页" icon={<LastPage />} onClick={() => handlePageSwitch(NEXT)} />
     </BottomNavigation>
   );
 };
