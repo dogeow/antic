42d76ce8acf932ce4262dea94ea875fd0bd00fb9
likunyan <lky216@gmail.com>
2023-05-27 23:07:09 +0800

    Update Menu.tsx

--- src/components/nav/Menu.tsx
+++ src/components/nav/Menu.tsx
@@ -2,19 +2,9 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import makeStyles from "@mui/styles/makeStyles";
 import * as React from "react";
 
 import { getHost } from "../../helpers";
-
-const useStyles = makeStyles(() => ({
-  card: {
-    minHeight: 152,
-  },
-  intro: {
-    wordWrap: "break-word",
-  },
-}));
 
 interface MenuProps {
   url: string;
@@ -22,17 +12,15 @@ interface MenuProps {
 }
 
 const Menu: React.FC<MenuProps> = ({ url, name }) => {
-  const classes = useStyles();
-
   return (
     <Grid item xs={12} sm={6} md={4} lg={3}>
       <a href={url} target="_blank" rel="noopener noreferrer">
-        <Card className={classes.card}>
+        <Card sx={{ minHeight: 152 }}>
           <CardContent>
-            <Typography variant="body1" component="h3" className={classes.intro}>
+            <Typography variant="body1" component="h3" sx={{ wordWrap: "break-word" }}>
               {name}
             </Typography>
-            <Typography color="textSecondary" className={classes.intro}>
+            <Typography color="textSecondary" sx={{ wordWrap: "break-word" }}>
               {getHost(url)}
             </Typography>
           </CardContent>
